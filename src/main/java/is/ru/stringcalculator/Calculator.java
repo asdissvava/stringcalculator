9d7e098cbd4d58f43eaeb65e2cdea9e80bc8dd13
asdissvava <asdish11@ru.is>
2015-10-18 12:05:01 +0000

    Added third test TwoNumbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -5,13 +5,15 @@ public class Calculator {
 	public static int add(String text){
 		if(text.equals("")){
 			return 0;
-		} /*
+		} 
 		else if(text.contains(",")){
-			return sum(splitNumbers(text));
-		} */
-		else
+			String [] numbers = text.split(",");
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		} 
+		else {
 			return 1;
 	}
+}
 /*
 	private static int toInt(String number){
 		return Integer.parseInt(number);

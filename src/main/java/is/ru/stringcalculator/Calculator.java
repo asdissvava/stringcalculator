669ae1e1931943e147626ce798c05d17309a49a3
asdissvava <asdish11@ru.is>
2015-10-18 19:05:35 +0000

    Added test SpecifiedDelimiter

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,9 +6,12 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		} 	
+		if(text.startsWith("//")){
+			return sum(splitByGiven(text));
+		} 
 		if(text.contains(",")){
 			return sum(splitNumbers(text));
-		} 
+		}
 		else
 			return 1;
 }
@@ -19,8 +22,13 @@ public class Calculator {
 
 	private static String[] splitNumbers(String numbers){
 	    return numbers.split("[,:\n]");
+	} 
+   private static String[] splitByGiven(String numbers){
+		String[] part = numbers.split("\n");
+		String delimiter = part[0].substring(2);
+		numbers = part[1]; 
+		return numbers.split(delimiter); 
 	}
-      
     private static int sum(String[] numbers){
  	    int total = 0;
         for(String number : numbers){
